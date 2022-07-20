a96f2d1a9777cbe0f6cfed2ac866e6be58c0ebd1
BrandonO-98 <brandonshew98@gmail.com>
2022-07-19 20:02:47 -0400

    Starting the Frontend. Index page as well as show page setup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Pixel Technologies</h1>
-      
-    </>
-  );
+    <Router>
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
